eca5e03c37122bc3af0a1af00ae1e1503b9d1ac1
Katona Gergő <geri.katona.mechwartsuli23@gmail.com>
2025-07-21 07:16:54 +0200

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -82,7 +82,7 @@ app.use("/api/current-song", currentSongRoute);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
+	app.get("{/*any}", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 	});
 }
